bd6b3748b66e58feff7554d9e5b6fc0a2487106f
phmazaris <ph.mazaris@gmail.com>
2026-03-28 15:23:43 +0100

    changed build.gradle.kts for sonarcloud setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("jacoco")
-    id("org.sonarqube") version "5.1.0.4882"
+    id("org.sonarqube")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    plugins {
-        id("org.sonarqube") version "7.2.3.7755"
-    }
 
-    sonar {
-        properties {
-            property("sonar.projectKey", "weltreise-dora-the-explorer_Weltreise_App")
-            property("sonar.organization", "weltreise-dora-the-explorer")
-        }
+
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "weltreise-dora-the-explorer_Weltreise_App")
+        property("sonar.organization", "weltreise-dora-the-explorer")
     }
 }
