13258dcc251bb3ed56fee9b7b3844f26b68f37a9
unknown <chmaizinger@edu.aau.at>
2026-04-10 22:05:25 +0200

    Relocate LoginScreen File

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/theme/LoginScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/theme/LoginScreen.kt
@@ -1,6 +1,5 @@
-package at.aau.serg.websocketbrokerdemo
+package at.aau.serg.websocketbrokerdemo.ui.theme
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,13 +9,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import at.aau.serg.websocketbrokerdemo.ui.theme.LobbyScreen
 import androidx.compose.material3.MaterialTheme
 
 
